9747a161dfcdee77dd13014d190d605731f214a4
AmSmo <adam.smolenski@gmail.com>
2020-10-17 10:36:13 -0400

    pre-jwt

--- app/models/floor.rb
+++ app/models/floor.rb
@@ -1,2 +1,4 @@
 class Floor < ApplicationRecord
+    has_many :floor_galleries
+    has_many :galleries, through: :floor_galleries
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,5 +1,6 @@
 class Gallery < ApplicationRecord
   belongs_to :user
+  has_one :floor_gallery
   has_many :gallery_paintings
   has_many :paintings, through: :gallery_paintings
 end
